3de3fa3cad84724391c4bbb8d62862b286939bae
0x-70da <mahmoudAnonymous0@gmail.com>
2025-09-26 22:08:12 +0300

    fixed toggle all complete function bug and presisted todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import ToDoForm from "./components/ToDoForm";
 import ToDoList from "./components/ToDoList";
 import ToDoActions from "./components/ToDoActions";
 
 function App() {
-  let [todos, setTodos] = useState([]);
+
+  const initialTodos = localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")): [];
+
+  let [todos, setTodos] = useState(initialTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  
+  const [todoShow, setTodoShow] = useState("all");
+  
+  const filteredTodos = useMemo(() => {
+    if(todoShow === "active"){
+      return todos.filter((todo) => !todo.complete);
+    }else if(todoShow === "complete"){
+      return todos.filter((todo) => todo.complete);
+    }
+    return todos;
+  }, [todos, todoShow]);
+  
   const addTodo = (todo) => {
     setTodos((prev) => [todo, ...prev]);
   };
   const onDelete = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
-
-  const [todoShow, setTodoShow] = useState("all");
-
-  if(todoShow === "active"){
-    todos = todos.filter((todo) => !todo.complete);
-  }else if(todoShow === "complete"){
-    todos = todos.filter((todo) => todo.complete);
-  }
 
   const toggleComplete = (id) => {
     setTodos((prevTodos) => prevTodos.map((todo) => {
@@ -36,18 +47,21 @@ function App() {
   }
 
   const toggleAllComplete = () => {
-    setTodos((prevTodos) => prevTodos.map((todo) => {
-      return {...todo, complete: !todo.complete};
-    }));
+    setTodos((prevTodos) =>{
+      const allComplete = prevTodos.every((todo) => todo.complete); 
+      return prevTodos.map((todo) => {
+      return {...todo, complete: !allComplete};
+    })});
   }
+
 
 
   return (
     <>
         <div className="main-container">
           <ToDoForm addTodo={addTodo} />
-          {todos.length ? (
-            <ToDoList todos={todos} onDelete={onDelete} toggleComplete={toggleComplete} />
+          {filteredTodos.length ? (
+            <ToDoList todos={filteredTodos} onDelete={onDelete} toggleComplete={toggleComplete} />
           ) : (
             <div>No Todos</div>
           )}
